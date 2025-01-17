6c4a900a45f525ebdaf5dad508492a012765f6ba
Laura Ibbotson <72210319+Lozzy-ibbotson@users.noreply.github.com>
2025-01-17 13:31:17 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using v4._8_Sqlite_Tester.Models;
+using SQLitePCL;
+
+namespace v4._8_Sqlite_Tester
+{
+    internal static class Program
+    {
+        
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            try
+            {
+                SqliteDbContext my_context = new SqliteDbContext();
+                Debug.WriteLine("create complete, operation begin");
+                Debug.WriteLine("DB created? : " + my_context.Database.EnsureCreated());
+
+                //test write new data to db
+                var site = new Site { Id = 2, Notes = "New Site" };
+                my_context.Sites.Add(site);
+                my_context.SaveChanges();
+                Debug.WriteLine("saved");
+
+                var sites = my_context.Sites.ToList();
+                Debug.WriteLine("Fetch site data");
+
+                foreach (var current_site in sites)
+                {
+                    Debug.WriteLine($"Site ID: {current_site.Id}, Notes: {current_site.Notes}");
+                }
+
+                //Winforms and DevExpress connection
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1());
+                
+
+                //clean up
+                my_context.Dispose();
+            }
+            catch (Exception ex) //debug caught exception
+            {
+                Debug.WriteLine(ex.Message);
+                MessageBox.Show(ex.Message); //keeps all debug info open
+            }
+        }
+    }
+}
---
+++ v48_sqlite_dbContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace v4._8_Sqlite_Tester
+{
+    public partial class v48_sqlite_dbContext : DbContext
+    {
+        public v48_sqlite_dbContext()
+        {
+        }
+
+        public v48_sqlite_dbContext(DbContextOptions<v48_sqlite_dbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlite("Data Source=v48_sqlite_db.db");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
